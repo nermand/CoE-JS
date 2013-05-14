0df5d4c7a08b9d7df077d6e360b0f13b94bb22c2
Nerman <tegoba@gmail.com>
2013-05-14 16:38:47 +0200

    Add inital files

--- MvcGit/Controllers/HomeController.cs
+++ MvcGit/Controllers/HomeController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using MvcGit.Models;
 
 namespace MvcGit.Controllers
 {
@@ -10,7 +11,7 @@ namespace MvcGit.Controllers
     {
         public ActionResult Index()
         {
-            ViewBag.Message = "Welcome to My World!";
+            ViewBag.Message = "Simple Example Of Knockout Bindings";
 
             return View();
         }
@@ -18,6 +19,39 @@ namespace MvcGit.Controllers
         public ActionResult About()
         {
             return View();
+        }
+
+        [HttpGet]
+        public JsonResult TaskList()
+        {
+            var taskList = new List<Task>
+                {
+                    new Task() {Title = "Learn Javascript Frameworks", TaskType = 0},
+                    new Task() {Title = "Master .Net MVC", TaskType = 2},
+                    new Task() {Title = "Get familiar with Git", TaskType = 1, IsDone = true}
+                };
+
+
+            return Json(taskList, JsonRequestBehavior.AllowGet);
+        }
+
+        [HttpGet]
+        public JsonResult GetTaskTypes()
+        {
+            var types = new List<TaskType>
+                {
+                    new TaskType() {Id = 0, Type = "Bug"},
+                    new TaskType() {Id = 1, Type = "Test"},
+                    new TaskType() {Id = 2, Type = "Enhancement"}
+                };
+
+            return Json(types, JsonRequestBehavior.AllowGet);
+        }
+
+        [HttpPost]
+        public JsonResult SaveForm(IEnumerable<Task> tasks)
+        {
+            return  Json(new {success = true, listSize= tasks.Count().ToString()});
         }
     }
 }
---
+++ MvcGit/Models/Task.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MvcGit.Models
+{
+    public class Task
+    {
+        public string Title { get; set; }
+        public bool IsDone { get; set; }
+        public int TaskType { get; set; }
+    }
+
+    public class TaskType
+    {
+        public string Type { get; set; }
+        public int Id { get; set; }
+    }
+}
